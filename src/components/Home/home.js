96fe0add5fc57d8a2e67f269dcccd2c5f808faa8
unknown <smitgor1620@gmail.com>
2020-04-07 18:19:30 +0530

    second commit

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import smit from "./smit.jpeg";
-import facebookIcon from "./facebook.png";
+
 import "./home.css";
-import { FaFacebookF } from "react-icons/fa";
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
 class Home extends Component {
   render() {
     return (
       <div className="Home">
+        <link
+          href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+          rel="stylesheet"
+        />
+
         <div className="Home-Header">
           <div className="Home-Design"></div>
           <div className="Home-Image">
@@ -19,12 +24,51 @@ class Home extends Component {
               <div className="Home-Header-Profile-2">Data Analyst</div>
             </div>
             <div className="Home-Header-Icon">
-              <div className="Facebook-Icon">
-                 <a href='https://www.facebook.com' target='_blank' rel="noopener noreferrer" ><FaFacebookF color='white'/></a>
-              </div>
-              <div className="Facebook-Icon">
-                 <a href='https://www.facebook.com' target='_blank ' rel="noopener noreferrer" ><FaFacebookF /></a>
-              </div>
+              <a
+                className="facebook-icon"
+                href="https://www.facebook.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a
+                className="linkedin-icon"
+                href="https://www.linkedin.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-linkedin-in"></i>
+              </a>
+              <a
+                className="github-icon"
+                href="https://www.linkedin.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i class="fab fa-github"></i>
+              </a>
+              <a
+                className="instagram-icon"
+                href="https://www.linkedin.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a
+                className="twitter-icon"
+                href="https://www.linkedin.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i class="fab fa-twitter"></i>
+              </a>
+            </div>
+            <div className="Arrow">
+              <a className='arrow-down' href='.About'>
+                <i class="fas fa-chevron-down"></i>
+              </a>
             </div>
           </div>
         </div>
